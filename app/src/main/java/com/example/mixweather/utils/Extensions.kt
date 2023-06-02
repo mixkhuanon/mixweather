c5128a98f84c997e1e5ca71fcf34620d3d7e8aec
KhunanonS <khunanon.mix@gmail.com>
2023-06-02 17:09:16 +0700

    - Create convertToDateTime.

--- app/src/main/java/com/example/mixweather/utils/Extensions.kt
+++ app/src/main/java/com/example/mixweather/utils/Extensions.kt
@@ -1,8 +1,18 @@
 package com.example.mixweather.utils
 
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.Date
+
 object Extensions {
 
     fun Double.toFahrenheit(): Double = ((this * 9 / 5) + 32)
-
     fun Double.toCelsius(): Double = ((this - 32) * 5 / 9)
+
+    @SuppressLint("SimpleDateFormat")
+    fun convertToDateTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("yyyy/MM/dd \nHH:mm")
+        return format.format(date)
+    }
 }
