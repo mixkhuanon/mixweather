79fa770781e7c0655a80e49a83502b4182e6af47
KhunanonS <khunanon.mix@gmail.com>
2023-06-02 17:07:56 +0700

    - Set Activity.

--- app/src/main/java/com/example/mixweather/landing/LandingActivity.kt
+++ app/src/main/java/com/example/mixweather/landing/LandingActivity.kt
@@ -2,18 +2,36 @@ package com.example.mixweather.landing
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.mixweather.R
 import com.example.mixweather.databinding.ActivityLanindingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LandingActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLanindingBinding
+    private var binding: ActivityLanindingBinding? = null
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLanindingBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding?.root)
+        initNav(savedInstanceState)
+    }
+
+    private fun initNav(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            val hostFragment = NavHostFragment.create(
+                R.navigation.weather_navigation
+            )
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, hostFragment)
+                .setPrimaryNavigationFragment(hostFragment)
+                .commitNow()
+            navController = hostFragment.navController
+        }
     }
 }
